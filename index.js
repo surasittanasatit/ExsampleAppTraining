1014c41235bbad00396f30edcf02b5bdc90d0731
S.Tanasatit <S.Tanasatit@hotmail.com>
2020-09-10 11:03:00 +0700

    Mik

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 //console.disableYellowBox = true
+LogBox.ignoreAllLogs();
 AppRegistry.registerComponent(appName, () => App);
